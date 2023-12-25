9b6537c623da514c357669b7fd73592556cc4151
athamour1 <thanosmour.tk@gmail.com>
2023-12-25 15:49:38 +0000

    Add: qCallendar extension

---
+++ frontend/src/pages/CallendarPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <q-page class="flex flex-center bg-background">
+
+    </q-page>
+</template>
+  
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'IndexPage'
+})
+</script>
+  
